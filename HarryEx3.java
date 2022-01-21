e7ee1777e141733ab78b02dda820877266f89cc5
Deyvant R_K_Bhardwaj <58858873+DeyvWeb@users.noreply.github.com>
2022-01-21 23:00:23 +0530

    Add files via upload

---
+++ HarryEx3.java
@@ -0,0 +1,99 @@
+// 1. What will be the result of the following expression?
+
+// float a = 7/4*9/2;
+
+import java.util.Scanner;
+
+// public class HarryEx3 {
+//     public static void main(String[] args) {
+//         float a = 7/4*9/2;
+//         System.out.println(a);
+        
+//     }
+    
+// }
+
+// 2. Write a java program to encrypt a grade by adding 8 to it. Decrypt it to show the correct grade.
+
+// public class HarryEx3{
+//     public static void main(String[] args) {
+
+//         char grade = 'B';
+//         grade = (char)(grade + 8);
+//         System.out.println(grade);
+//     }
+// }
+
+// 3.Use comparison operators to find out whether a given number is greater than the user entered number or not.
+
+// public class HarryEx3{
+//     public static void main(String[] args) {
+
+//         Scanner num = new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         byte n = num.nextByte();
+
+//         int a = 10;
+
+//         if(n>a)
+//         {
+//             System.out.print("Number is greater than the given number");
+//         }
+
+//         else if(n==a)
+//         {
+//             System.out.print("Number is same as the given number");
+//         }
+//         else
+//         {
+//             System.out.print("Number is less than the given number ");
+//         }
+
+//     }
+// }
+
+
+// 4. Write the following express in a java program.
+
+// (v^2-u^2)/2as
+
+
+// public class HarryEx3 {
+//     public static void main(String[] args) {
+//         Scanner num = new Scanner(System.in);
+//         System.out.println("Enter v number");
+//         double v = num.nextDouble();
+
+//         System.out.println("Enter u number");
+//         double u = num.nextDouble();
+
+//         System.out.println("Enter a number");
+//         double a = num.nextDouble();
+
+        
+//         System.out.println("Enter a number");
+//         double s = num.nextDouble();
+
+//         double x = ((v*v)-(u*u))/(2*a*s);
+//         System.out.println(x);
+        
+//     }
+    
+// }
+
+
+// 5. Find the value of the following expression :
+
+// int x = 7
+// int a = 7*49/7+35/7 
+
+
+public class HarryEx3 {
+    public static void main(String[] args) {
+        int x = 7;
+int a = 7*49/7+35/7; 
+        System.out.println(a);
+        
+    }
+    
+}
